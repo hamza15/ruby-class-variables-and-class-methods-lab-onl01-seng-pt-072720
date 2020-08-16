24362fa30f29fd6f28c2c94fe98af09c40200681
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-16 19:39:05 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+class Song
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_coint = {}
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre 
+    @@count += 1 
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def artist
+    @artist
+  end 
+  
+  def genre
+    @genre 
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.artists
+    
+  end 
+  
+  def self.genres
+    
+  end 
+  
+  def self.genre_count
+  
+  end
+  
+  def self.artist_count
+  
+  end
+  
+  
+end 
