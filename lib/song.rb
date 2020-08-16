cf7e56fce07cf3b574184a5430ce5530f85c2b10
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-16 20:20:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,7 +11,7 @@ class Song
     @genre = genre
     @@count += 1
     @@artists.push(artist) unless @@artists.include?(artist)
-    @@genres.push(genre) unless @@genres.include?(genre)
+    @@genres.push(genre)
   end 
   
   def name
