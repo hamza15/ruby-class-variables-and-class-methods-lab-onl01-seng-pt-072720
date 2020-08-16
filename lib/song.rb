fe8c60cfd9f2bb3363e1e1699feae1f48d1b31a2
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-08-16 21:21:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,12 +52,12 @@ class Song
   def self.artist_count
     @@artists.each do |artist|
       if @@artist_count.key?(artist)
-        @@genre_count[genre] += 1
+        @@artist_count[artist] += 1
       else
-        @@genre_count[genre] = 1
+        @@artist_count[artist] = 1
       end
     end
-    @@genre_count
+    @@artist_count
   end
   
   
